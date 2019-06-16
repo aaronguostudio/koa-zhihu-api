5f05debea0dcc34458847059ca9e8971d071e58e
Aaron Guo <aaronguostudio@gmail.com>
2019-06-16 14:20:44 -0600

    add routes

--- index.js
+++ index.js
@@ -1,22 +1,43 @@
 'use strict'
 const Koa = require('koa')
+const Router = require('koa-router')
 const app = new Koa()
+const router = new Router()
+const usersRouter = new Router({prefix: '/users'})
 
-app.use(async (ctx, next) => {
-  console.log('middle 1')
-  await next();
-  console.log('middle 5')
-  ctx.body = 'Hello Zhihu API'
+router.get('/', ctx => {
+  ctx.body = 'Index'
 })
 
-app.use(async (ctx, next) => {
-  console.log('middle 2')
-  await next();
-  console.log('middle 4')
+usersRouter.get('/', ctx => {
+  ctx.body = 'User Index'
 })
 
-app.use(async (ctx, next) => {
-  console.log('middle 3')
+usersRouter.post('/', ctx => {
+  ctx.body = 'User Post'
 })
 
-app.listen(3000)
+usersRouter.get('/:id', ctx => {
+  ctx.body = 'User ' + ctx.params.id
+})
+
+app.use(router.routes())
+app.use(usersRouter.routes())
+
+// custom router
+// app.use(async (ctx, next) => {
+//   if (ctx.url === '/') {
+//     if (ctx.method === 'GET') ctx.body='index#GET'
+//     else if (ctx.method === 'POST') ctx.body='index#POST'
+//     else ctx.status = 405
+//   }
+//   else if (ctx.url.match(/\/users\/\w+/)) {
+//     const userId = ctx.url.match(/\/users\/(\w+)/)[1];
+//     ctx.body = `User ${userId}`
+//   }
+//   else {
+//     ctx.status = 404
+//   }
+// })
+
+app.listen(3001)
