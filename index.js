734ebd39c001d43572da73232dc38255078d4647
Aaron Guo <aaronguostudio@gmail.com>
2019-06-16 13:50:15 -0600

    Add onion model

--- index.js
+++ index.js
@@ -2,8 +2,21 @@
 const Koa = require('koa')
 const app = new Koa()
 
-app.use((ctx) => {
+app.use(async (ctx, next) => {
+  console.log('middle 1')
+  await next();
+  console.log('middle 5')
   ctx.body = 'Hello Zhihu API'
+})
+
+app.use(async (ctx, next) => {
+  console.log('middle 2')
+  await next();
+  console.log('middle 4')
+})
+
+app.use(async (ctx, next) => {
+  console.log('middle 3')
 })
 
 app.listen(3000)
