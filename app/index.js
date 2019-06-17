6c4bb06194772beb5d3ac4778686dad750d3bf13
Aaron Guo <aaronguostudio@gmail.com>
2019-06-17 17:50:41 -0600

    Add upload file

--- app/index.js
+++ app/index.js
@@ -1,22 +1,32 @@
 'use strict'
 const Koa = require('koa')
-const bodyparser = require('koa-bodyparser')
+const koaBody = require('koa-body')
+const koaStatic = require('koa-static')
 const error = require('koa-json-error')
 const parameter = require('koa-parameter')
 const mongoose = require('mongoose')
 const app = new Koa()
 const routers = require('./routes')
 const { db } = require('./config')
+const path = require('path')
 
 mongoose.connect(db, { useNewUrlParser: true }, () => {
   console.log('> db connect')
 })
 mongoose.connection.on('error', console.error)
 
+app.use(koaStatic(path.join(__dirname, 'public')))
 app.use(error({
   postFormat: (e, {stack, ...rest}) => process.env.NODE_ENV === 'production' ? rest : { stack, ...rest }
 }))
-app.use(bodyparser())
+app.use(koaBody({
+    multipart: true,
+    formidable: {
+      uploadDir: path.join(__dirname, '/public/uploads'),
+      keepExtensions: true
+    }
+  }
+))
 app.use(parameter(app))
 routers(app)
 
